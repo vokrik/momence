7661f2c9019b71b323ae385c815a619f1a056ba3
vokrik <vokrik@gmail.com>
2023-11-19 21:15:44 +0100

    Moves API doc to TS-file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 //Importing project dependancies that we installed earlier
 import * as dotenv from "dotenv";
+import apidoc from "./apidoc";
 import errorHandlerMiddleware from "./errorHandlerMiddleware";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import path from "path";
 import cors from "cors";
 import helmet from "helmet";
 
@@ -17,11 +17,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(
-  "/api/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(YAML.load(path.join(__dirname, "/apidoc.yml"))),
-);
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(apidoc));
 app.use("/api/exchange-rates/", exchangeRatesRouter);
 
 app.use(errorHandlerMiddleware);
